f8e67ac8486b55e0c30afbd7e39df0fcf5089aaf
SPRandhawa <jaibirsim@gmail.com>
2026-07-20 11:55:23 +0530

    Update apps.py

--- data_project/accounts/apps.py
+++ data_project/accounts/apps.py
@@ -1,8 +1,21 @@
 from django.apps import AppConfig
+   import os
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
 
     def ready(self):
-        import accounts.signals   # ✅ THIS LINE IS IMPORTANT
+        import accounts.signals
+
+        # ✅ SAFE PLACE TO CREATE SUPERUSER
+     
+        if os.environ.get("CREATE_SUPERUSER") == "True":
+            from django.contrib.auth.models import User
+
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    email="admin@gmail.com",
+                    password="admin123"
+                )
