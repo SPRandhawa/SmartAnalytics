088963bf38272b5a12e3f1a2981daa4fdc145746
SPRandhawa <jaibirsim@gmail.com>
2026-07-20 16:49:36 +0530

    Update signals.py

--- data_project/accounts/signals.py
+++ data_project/accounts/signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_migrate
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
+import os
 
 
 @receiver(post_save, sender=User)
@@ -14,3 +15,18 @@ def create_profile(sender, instance, created, **kwargs):
 def save_profile(sender, instance, **kwargs):
     if hasattr(instance, 'profile'):
         instance.profile.save()
+
+
+@receiver(post_migrate)
+def create_superuser(sender, **kwargs):
+    # ✅ RUN ONLY FOR YOUR APP
+    if sender.name != "accounts":
+        return
+
+    if os.environ.get("CREATE_SUPERUSER") == "True":
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@gmail.com",
+                password="admin123"
+            )
